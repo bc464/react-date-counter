62cc0f7bdb2d1c158fa99c3be32e68ee7bd8a40c
bc464 <fil419@webmail.co.za>
2023-07-18 16:58:09 +0200

    first commit project files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Hello CodeSandBox</h1>
+      <h2>Start editing to see some magic happening</h2>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const date = new Date(" june 21 2027");
+  date.setDate(date.getDate() + count);
+
+  function handleStepDown() {
+    setStep((s) => s - 1);
+  }
+  function handleStepUp() {
+    setStep((s) => s + 1);
+  }
+  function handleCountDown() {
+    setCount((c) => c - step);
+  }
+  function handleCountUp() {
+    setCount((c) => c + step);
+  }
+
+  return (
+    <>
+      <div className="containers">
+        <button onClick={handleStepDown}>-</button>
+        <h2>Step: {step}</h2>
+        <button onClick={handleStepUp}>+</button>
+      </div>
+
+      <div className="containers">
+        <button onClick={handleCountDown}>-</button>
+        <h2>Count: {count}</h2>
+        <button onClick={handleCountUp}>+</button>
+      </div>
+
+      <h4>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        {date.toDateString()}
+      </h4>
+    </>
+  );
+}
